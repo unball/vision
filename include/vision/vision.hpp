5b8ecbb48af66ebec67e356d43db5bb316aee29c
gabrielnaves <gabri.navess@gmail.com>
2016-04-25 22:57:55 -0300

    Added the vision files to this repo (they were removed from the main repository)

---
+++ include/vision/vision.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file   vision.hpp
+ * @author Gabriel Naves da Silva
+ * @date   27/02/2016
+ *
+ * @attention Copyright (C) 2014 UnBall Robot Soccer Team
+ *
+ * @brief  Vision class
+ *
+ * Defines computer vision class
+ */
+
+#ifndef UNBALL_VISION_H_
+#define UNBALL_VISION_H_
+
+#include <vector>
+
+#include <ros/ros.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <vision/segmenter.hpp>
+#include <vision/tracker.hpp>
+
+class Vision
+{
+  public:
+    static Vision& getInstance();
+
+    void run();
+};
+
+#endif // UNBALL_VISION_H_
