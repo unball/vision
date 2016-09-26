b8701d61f7506bcc0baf75b7616448c7f236c851
Vieira <vieiranetoc@gmail.com>
2016-09-26 20:45:55 -0300

    edited class to only Canny input matrix

--- src/vision/findrobots.cpp
+++ src/vision/findrobots.cpp
@@ -3,12 +3,17 @@
 REGISTER_ALGORITHM_DEF(FindRobots);
 
 void FindRobots::run(){
-    cv::Mat input;
-    auto upperPoints = preProcessor(input);
-    cv::imshow("Canny", upperPoints);
+    RawImage::getInstance().getRawDepthImage().copyTo(input_);
+    auto upperPoints = preProcessor(input_);
+    cv::imshow(window_name_, upperPoints);
+    if(cv::waitKey(30) == 'r')
+        cv::destroyWindow(window_name_);
 }
 
 void FindRobots::init(){
+    cv::namedWindow(window_name_);
+    cv::createTrackbar("Canny Thresh 1", window_name_, &cannythresh1_, 100);
+    cv::createTrackbar("Canny Thresh 2", window_name_, &cannythresh2_, 300);
 
 }
 
@@ -21,25 +26,6 @@ cv::Mat FindRobots::preProcessor(cv::Mat input){
     cv::cvtColor(cannyMat, cannyMat, CV_BGR2HSV);
     cv::split(cannyMat, channels);
     
-    cv::Canny(channels[2], cannyMat, _cannythresh1, _cannythresh2, 3);
-    cannyMat = derivate(cannyMat);
+    cv::Canny(channels[2], cannyMat, cannythresh1_, cannythresh2_, 3);
     return cannyMat;
-}
-
-void FindRobots::extractPoints(cv::Mat& upperPoints){
-    auto color = cv::Scalar::all(0);
-    cv::drawContours(upperPoints, params_.contours, params_.largestAreaIndex, color, 6, 10, params_.hierarchy);
-    int const max_kernel_size = 21;
-
-    cv::createTrackbar( "Kernel size:\n 2n +1", "RGB Video", &erosion_size, max_kernel_size);
-    cv::Mat element = cv::getStructuringElement( cv::MORPH_RECT,
-                                       cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                       cv::Point( erosion_size, erosion_size ) );
-    cv::createTrackbar( "Kernel size 2:\n 2n +1", "RGB Video", &erosion_size2, max_kernel_size);
-
-    cv::Mat element2 = getStructuringElement( cv::MORPH_RECT,
-                                       cv::Size( 2*erosion_size2 + 1, 2*erosion_size2+1 ),
-                                       cv::Point( erosion_size2, erosion_size2 ) );
-    cv::dilate(upperPoints, upperPoints, element);
-    cv::erode(upperPoints, upperPoints, element2);
 }
