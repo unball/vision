0c1bad4337440f1b259ec1a69e3b7d064e57cddc
Samuel Venzi <samuel.venzi@me.com>
2017-08-24 08:58:46 -0300

    Kalman topic now receiving message

--- kalman.py
+++ kalman.py
@@ -8,27 +8,20 @@ def main():
     rospy.init_node('kalman_topic', anonymous=True)
 
     while not rospy.is_shutdown():
-        rospy.Subscriber('vision_topic', VisionMessage, subscriber, queue_size=1)
-        # rospy.spin()
+        rospy.Subscriber('pixel_to_metric_conversion_topic', VisionMessage, subscriber)
+        rospy.spin()
     pass
 
 
-def subscriber(msg):
+def subscriber(data):
     
     rate = rospy.Rate(10)
-
-   
-    msg = VisionMessage()
-    x_curr = msg.x
-    y_curr = msg.y
-    th_curr = msg.th
-    ballx_curr = msg.ball_x
-    bally_curr = msg.ball_y
-
-    print x_curr
-    print y_curr
-    print bally_curr
-
+    
+    x_curr = data.x
+    y_curr = data.y
+    th_curr = data.th
+    ballx_curr = data.ball_x
+    bally_curr = data.ball_y
 
     rate.sleep()
 
